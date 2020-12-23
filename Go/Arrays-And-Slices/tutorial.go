7e3502c1b97c89cd1484597013f638e6292fe16b
Ryan Tan <ryan.tan@circles.asia>
2020-12-23 16:13:10 +0800

    Arrays are values example

--- Go/Arrays-And-Slices/tutorial.go
+++ Go/Arrays-And-Slices/tutorial.go
@@ -28,6 +28,31 @@ func main() {
 	msg = "Slice index 0 modified again"
 	slice[0] = 20
 	print(msg, arr, slice)
+
+	fmt.Println("arr2 references arr and arr2[0] is set to 17")
+	arr2 := arr
+	arr2[0] = 17
+	fmt.Printf("arr: %v\n", arr)
+	fmt.Printf("arr2: %v\n", arr2)
+
+	fmt.Println("Creating arrArr and arrArr2 as {arr,arr2}")
+	arrArr := [2][5]int{arr, arr2}
+	arrArr2 := [2][5]int{arr, arr2}
+
+	fmt.Println("Arrays are values >>> Deep Copy")
+	fmt.Println("Assigning arrArr[0][0] to 23")
+	arrArr[0][0] = 23
+	fmt.Printf("arrArr: %v\n", arrArr)
+	fmt.Printf("arrArr2: %v\n", arrArr2)
+	fmt.Printf("arr: %v\n", arr)
+	fmt.Printf("arr2: %v\n", arr2)
+
+	fmt.Println("Assigning arrArr[0][0] to 37")
+	arrArr2[1][0] = 37
+	fmt.Printf("arrArr: %v\n", arrArr)
+	fmt.Printf("arrArr2: %v\n", arrArr2)
+	fmt.Printf("arr: %v\n", arr)
+	fmt.Printf("arr2: %v\n", arr2)
 }
 
 func print(msg string, arr [5]int, slice []int) {
