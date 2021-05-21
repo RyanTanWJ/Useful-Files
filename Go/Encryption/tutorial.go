eb56f836dab580849f52f7db3236c6d009493b46
Ryan Tan <ryan.tan@circles.asia>
2021-05-21 15:36:18 +0800

    Go Encryption Tutorial

---
+++ Go/Encryption/tutorial.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+const (
+	textToEncrypt = "Encrypt this"
+	password      = "password"
+)
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	hashedPass := createHash(passphrase)
+
+	// Create cipher
+	block, err := aes.NewCipher([]byte(hashedPass))
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err)
+	}
+
+	//Create nonce
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		panic(err)
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func decrypt(data []byte, passphrase string) string {
+	hashedPass := createHash(passphrase)
+	key := []byte(hashedPass)
+
+	// Create cipher
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err)
+	}
+
+	nonce, ciphertext := data[:gcm.NonceSize()], data[gcm.NonceSize():]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err)
+	}
+	return string(plaintext)
+}
+
+func main() {
+	// Encrypt
+	ciphertext := encrypt([]byte("Encrypt this"), "password")
+	fmt.Printf("My encrypted plaintext:\n%s\n", ciphertext)
+
+	// Decrypt
+	plaintext := decrypt(ciphertext, password)
+	fmt.Printf("My decrypted ciphertext:\n%s\n", plaintext)
+}
