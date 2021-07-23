32294ffeb5d6912dfa986cd9ab2386f089df68b4
Ryan Tan <ryan.tan@circles.asia>
2021-07-23 22:22:28 +0800

    Restructure Java folder and added Spring folder

---
+++ Java/Spring/ryan-user-service/src/main/java/com/ryan/user/service/ryanuserservice/RyanUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.user.service.ryanuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyanUserServiceApplication.class, args);
+	}
+
+}
---
+++ Java/Spring/ryan-user-service/src/test/java/com/ryan/user/service/ryanuserservice/RyanUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.user.service.ryanuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
