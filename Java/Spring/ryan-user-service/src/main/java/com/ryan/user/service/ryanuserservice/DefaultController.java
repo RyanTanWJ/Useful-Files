ecd1976aaa5b46b5382a4bd5cc7a47d047c2d89a
Ryan Tan <ryan.tan@circles.asia>
2021-07-23 23:01:49 +0800

    Removed health controller
    Handled by Actuator

--- Java/Spring/ryan-user-service/src/main/java/com/ryan/user/service/ryanuserservice/DefaultController.java
+++ Java/Spring/ryan-user-service/src/main/java/com/ryan/user/service/ryanuserservice/DefaultController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthController {
-    @GetMapping("/health")
+public class DefaultController {
+    @GetMapping("/")
     public String index() {
-        return "I am healthy!\n";
+        return "Welcome to Ryan's User Service!\n";
     }
 
 }
